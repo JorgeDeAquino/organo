fbaceadc782f80d14b3d52bf41c074f571d9be5e
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-12-31 17:37:13 -0300

    add state de nome, cargo e imagem

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,7 @@
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../LIstaSuspensa'
+import { useState } from 'react'
 import './Formulario.css'
 
 const Formulario = () => {
@@ -15,18 +16,39 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Foi submetido')
+        console.log('Foi submetido', nome, cargo, imagem)
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite a URL da imagem" />
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Nome" 
+                placeholder="Digite seu nome"
+                valor={nome}
+                aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Cargo" 
+                placeholder="Digite seu cargo"
+                valor={cargo}
+                aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto 
+                label="Imagem" 
+                placeholder="Digite a URL da imagem" 
+                valor={imagem}
+                aoAlterado={valor => setImagem(valor)}
+                />
                 <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
                 <Botao>
                     Criar Card
