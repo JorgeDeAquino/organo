4e03e9b1f68e96485707a7d08fd938cb56c09f65
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-09 12:47:58 -0300

    add organizacao cards

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/JorgeDeAquino.png' alt='Jorge Alexandre' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Jorge Alexandre</h4>
-                <h5>Programedor Front-End</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
