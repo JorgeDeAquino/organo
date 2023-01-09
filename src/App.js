1f40d3d3ef9d01cd3fc039fee79b2b2be52cccde
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-09 15:28:13 -0300

    add detalhes ao card e add key para colaborador

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
   const [colaboradores, setColaboradores]  = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
